3e02fe5439683f74c6c60b18b4ff50946e2b26ae
Dan Sun <dansun47@gmail.com>
2018-01-18 19:40:50 -0500

    reorganized expSetup script to better accomodate change of directory name, added stdout output to summarize experiment setup

--- expSetup.sh
+++ expSetup.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
+DEFAULT_CPU_TO_TURNOFF="8 9 10" 
+DEFAULT_FREQUENCY=2.10GHz
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SYSTEM_SCRIPT_DIR=${DIR}/system_scripts
 #turn off puppet agent
 #sudo puppet agent --disable "running experiment"
 
 #turn on all cores
-sudo ~/scripts/bash_scripts/enable_cores.sh
+sudo ${SYSTEM_SCRIPT_DIR}/enable_cores.sh
 
-#turn off core 8,9,10
-#because ls application runs on core 3-7
-sudo ~/scripts/bash_scripts/turnoff_cpu.sh 8 9 10
+#turn off cores as indicated
+CPU_TO_TURNOFF=""
+if [ -z "$1" ]
+  then
+    CPU_TO_TURNOFF=${DEFAULT_CPU_TO_TURNOFF}
+  else
+    CPU_TO_TURNOFF="$1"
+fi
+sudo ${SYSTEM_SCRIPT_DIR}/turnoff_cpu.sh ${CPU_TO_TURNOFF}
 
 #change sudo time windows
 #sudo visudo
 
-
 #disable c-states
-sudo ~/scripts/bash_scripts/set_cstate.sh 1 0 
+sudo ${SYSTEM_SCRIPT_DIR}/set_cstate.sh 1 0 
 
 #turn off turbo boost
-sudo ~/scripts/bash_scripts/turbo-boost.sh disable
+sudo ${SYSTEM_SCRIPT_DIR}/turbo-boost.sh disable
 
+#set core frequency
+CPU_FREQUENCY=""
+if [ -z "$2" ]
+  then
+    CPU_FREQUENCY=DEFAULT_FREQUENCY
+  else
+    CPU_FREQUENCY="$2"
+fi
 sudo cpupower frequency-set -f 2.10GHz
+echo "Experiment Setup:"
+echo "All logical cores are turned on"
+echo "Logical cores ${CPU_TO_TURNOFF} are turned off"
+echo "C states are disabled"
+echo "Turboboost turned off"
+echo "Core frequency set to ${CPU_FREQUENCY}"
