a6f0077a18af271615703f29dc2f7d5d06b30ce5
Dan Sun <ds318@duke.edu>
2018-02-15 22:38:54 -0500

    finished writing

--- experiment_scripts/sparkCoreFreqChangeEffect.sh
+++ experiment_scripts/sparkCoreFreqChangeEffect.sh
@@ -23,7 +23,7 @@ fi
 
 if ! [ -d ${SCRIPT_HOME} ]
 	then
-	echo "SCRIPT_HOME path ${SCRIPT_HOME} does not exist"
+	echo "SCRIPT_HOME path ${wait $(cat server_connection.pid)SCRIPT_HOME} does not exist"
 	exit 1
 fi
 
@@ -45,7 +45,27 @@ ${SPARK_LOCAL_MODE_HOME}/run_${SPARK_APPLICATION}.sh ${SPARK_CORES}
 #wait for 30s
 echo "Waiting for 30 seconds..."
 sleep 30s
-
+#start moses
+echo "Starting moses"
+#parameters
+SERVER_THREADS=2
+QPS=500
+MAXREQS=$((400 * ${QPS}))
+WARMUPREQS=$((50 * ${QPS}))
+LAUNCH_SERVER_SCRIPT_CORE=4
+#call other script
+taskset -c ${LAUNCH_SERVER_SCRIPT_CORE} ${SCRIPT_HOME}/characterization_scripts/server.sh ${SERVER_THREADS} ${MAXREQS} ${WARMUPREQS} &	
+echo $! > server.pid
+#wait for 6 minutes
+echo "Waiting for 6 minutes..."
+sleep 6m
+#change core frequency of batch cores
+SECOND_FREQUENCY=1.20GHz
+echo "Changing SPARK_CORES ${SPARK_CORES} to frequency ${SECOND_FREQUENCY}"
+sudo cpupower -c ${SPARK_CORES} frequency-set -f ${SECOND_FREQUENCY}
+#wait for server
+echo "Waiting for server on process $(cat server.pid) to finish..."
+wait $(cat server.pid)
 #kill spark job
 echo "Killing spark jobs"
 ${SCRIPT_HOME}/kill_spark_job.sh
