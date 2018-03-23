0cfb3141bb02fee29c5c65ae550298baae94966c
Dan Sun <ds318@duke.edu>
2018-03-23 09:10:00 -0400

    added script to plot effect of frequency change

---
+++ data_analysis_scripts/plot_effect_of_freq_change.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import helpers
+import re #for splitting with multiple delimiters
+import BinAnalysis
+
+
+##################################data structure
+datas = {} 
+##################################helpers
+# def findThreadsAndQPS(filename):
+# 	actualFileName = filename.strip().split('/')[-1]
+# 	# print actualFileName
+# 	delimiters = 'q', 't', '.'
+# 	regexPattern = '|'.join(map(re.escape, delimiters))
+# 	# print regexPattern
+# 	splitted = re.split(regexPattern, actualFileName)
+# 	# print splitted
+# 	thread = splitted[1]
+# 	qps = splitted[2]
+# 	# print "Thread: ",thread
+# 	# print "Qps: ",qps
+# 	return (thread,qps)
+# def addThreadIntoDataIfNotAlreadyExist(thread):
+# 	if thread not in datas:
+# 		datas[thread] = {}
+# 		datas[thread]['qps'] = []
+# 		datas[thread]['95thLatency'] = []
+# def rearrangeByQps(originalQps, originalLatency, sortedQps, sortedLatency):
+# 	while not len(originalQps) == 0:
+# 		minimumQps = min(originalQps)
+# 		index = originalQps.index(minimumQps)	
+# 		correspondingLatency = originalLatency[index]
+# 		originalQps.remove(minimumQps)
+# 		originalLatency.remove(correspondingLatency)
+# 		sortedQps.append(minimumQps)
+# 		sortedLatency.append(correspondingLatency)	
+def plotLatencyVsId(latencyList, idList, inputFile):
+	#plotting
+	fig,ax = plt.subplots()
+	linesNumber = 0
+
+	ax.plot(idList,latencyList,helpers.getBuiltInColor(linesNumber))
+	fig.suptitle('Latency vs Id')
+	ax.set_xlabel('Id')
+	ax.set_ylabel('Latency')
+
+	# if x_limit > 0:
+		# ax.set_xlim([0, x_limit])
+
+	# ax.set_ylim([0 , percentile])
+	# plt.legend()
+
+	if save:
+		figureFilePath = helpers.getDir(inputFile)+'latencyVsId.jpg'
+		print 'Saving plot to ' + figureFilePath
+		fig.savefig(figureFilePath, dpi=1200)
+	else:
+		plt.show()
+		plt.close(fig)
+
+def plotServiceTimeVsStartTime(ServiceList, startList, inputFile):
+	#plotting
+	fig,ax = plt.subplots()
+	linesNumber = 0
+
+	ax.plot(startList,ServiceList,helpers.getBuiltInColor(linesNumber))
+	fig.suptitle('Service Time vs Service Start Time')
+	ax.set_xlabel('Service Start Time')
+	ax.set_ylabel('Service Time')
+
+	# if x_limit > 0:
+		# ax.set_xlim([0, x_limit])
+
+	# ax.set_ylim([0 , percentile])
+	# plt.legend()
+
+	if save:
+		figureFilePath = helpers.getDir(inputFile)+'serviceVsStart.jpg'
+		print 'Saving plot to ' + figureFilePath
+		fig.savefig(figureFilePath, dpi=1200)
+	else:
+		plt.show()
+		plt.close(fig)
+######################################
+saving = False
+if '-s' in sys.argv:
+	saving = True
+	sys.argv.remove('-s')
+
+bin_analysis = BinAnalysis.BinAnalysis()
+
+#configurable 
+percentile = 1
+x_limit = -1
+exclude_percentage = 0
+save = False
+figureFilePath = ''
+# respond to command line arguments
+if '-h' in sys.argv:
+	print '--percentile=percentile'
+	print '--x_limit=xlimit'
+	print '--exclude=front_portion_of_data'
+	exit()
+suppliedArguments = list(sys.argv)
+for argument in suppliedArguments:
+	if '--percentile=' in argument:
+		percentile = float((argument.strip().split('='))[1])
+		print 'Plotting up to ' + str(percentile) + " percentile"
+
+	if '--x_limit=' in argument:
+		x_limit = float((argument.strip().split('='))[1])
+		print 'Plotting with x_limit set to ' + str(x_limit)
+
+	if '--exclude=' in argument:
+		exclude_percentage = float((argument.strip().split('='))[1])
+		print 'excluding the first ' + str(exclude_percentage*100)+"% data"
+
+	if '--save' in argument:
+		save = True
+		sys.argv.remove('--save')
+		# figureFilePath = helpers.getDir(input_file)+'latency_distribution.jpg'
+		# print 'Saving plot to ' + figureFilePath
+
+# actual parsing
+
+for input_file in sys.argv[1:]:
+	binFile = input_file
+	# thread, qps = findThreadsAndQPS(input_file)
+	# addThreadIntoDataIfNotAlreadyExist(thread)
+	bin_analysis.readBinFile(binFile)
+	# Get necessary data
+	latencyList = bin_analysis.getList('latency')
+	idList = bin_analysis.getList('id')
+	serviceStartList = bin_analysis.getList('service_start_time')
+	serviceTimeList = bin_analysis.getList('service_time')
+	#plot
+	plotLatencyVsId(latencyList, idList, binFile)
+	plotServiceTimeVsStartTime(serviceTimeList, serviceStartList, binFile)
+
+	#clear, get ready for next parse
+	bin_analysis.clearData()
+
+
+
+
+###############################################################
