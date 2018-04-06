76a1bf90a1cabf4c0dc785a3a5aea4a83b07004f
Dan Sun <ds318@duke.edu>
2018-04-06 10:05:26 -0400

    changed plotting service/latency vs id so that figure is displayed instead of saved

--- data_analysis_scripts/plot_service_latency_time_vs_id.py
+++ data_analysis_scripts/plot_service_latency_time_vs_id.py
@@ -18,9 +18,11 @@ for bin_file in sys.argv[1:]:
 	ax.set_xlabel('request id')
 	ax.set_ylabel('time (ns)')
 	plt.legend()
-	file_path_without_extension = helpers.getRidOfLastFewCharacters(bin_file, 4)
-	save_pic_path =  file_path_without_extension + '_time_vs_id.jpg'
-	fig.savefig(save_pic_path, dpi=1200)
+	# file_path_without_extension = helpers.getRidOfLastFewCharacters(bin_file, 4)
+	# save_pic_path =  file_path_without_extension + '_time_vs_id.jpg'
+	# fig.savefig(save_pic_path, dpi=1200)
+	# plt.close(fig)
+	plt.show()
 	plt.close(fig)
 	bin_analysis.clearData()
 
