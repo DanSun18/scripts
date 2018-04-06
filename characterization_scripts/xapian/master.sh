3d614da9d0e3d96889d8110a8d5f0de45d8657d9
Dan Sun <ds318@duke.edu>
2018-04-06 01:04:07 -0400

    wrote master for running xapian experiments;moved outdated scripts into archive folder

---
+++ characterization_scripts/xapian/master.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#run on the client machine
+
+# First argument is the number of threads for server
+# Second argument is QPS
+
+source  /home/ds318/scripts/bash_helpers/readPathsConfiguration.sh
+
+if ! [ -d ${XAPIAN_HOME} ]
+	then
+	echo "XAPIAN_HOME path ${XAPIAN_HOME} does not exist"
+	exit 1
+fi
+
+if ! [ -d ${SCRIPT_HOME} ]
+	then
+	echo "SCRIPT_HOME path ${SCRIPT_HOME} does not exist"
+	exit 1
+fi
+
+source /home/ds318/scripts/bash_helpers/checkInputs.sh 2 "$#" "SERVER_THREADS, QPS" 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# constants
+SERVER_MACHINE=bcl15-cmp-00.egr.duke.edu
+LAUNCH_SERVER_SCRIPT_CORE=4
+
+LAUNCH_CLIENT_SCRIPT_CORE=4
+CLIENT_CORES=5-7
+CLIENT_THREADS=1
+
+#inputs 
+
+SERVER_THREADS=$1
+QPS=$2
+
+#launch server
+MAXREQS=$((300 * ${QPS}))
+WARMUPREQS=1000
+echo "--Starting server on ${SERVER_MACHINE}"
+ssh ds318@${SERVER_MACHINE} \
+"taskset -c ${LAUNCH_SERVER_SCRIPT_CORE} ${SCRIPT_HOME}/characterization_scripts/xapian/run_server.sh \
+	${SERVER_THREADS} ${MAXREQS} ${WARMUPREQS}" &	
+	#"sudo taskset -c ${LAUNCH_SERVER_SCRIPT_CORE} ${SCRIPT_HOME}/characterization_scripts/server.sh ${SERVER_THREADS} ${MAXREQS} ${WARMUPREQS} ${SERVER_CORES}" &
+echo $! > server_connection.pid 
+sleep 5s #wait for server to start up
+
+#launch client
+echo "--Starting client locally" 
+taskset -c ${LAUNCH_CLIENT_SCRIPT_CORE} ${SCRIPT_HOME}/characterization_scripts/xapian/run_client.sh \
+	${QPS} ${CLIENT_THREADS} ${CLIENT_CORES} ${SERVER_MACHINE}
+
+echo "--Waiting for server..."
+wait $(cat server_connection.pid)
+echo "--QPS = ${QPS} completed"
+
+#move data stored on client machine
+sleep 5s #wait for client to dump stats
+echo "moving data"
+DATADIR=/home/ds318/data
+mv lats.bin ${DATADIR}/t${SERVER_THREADS}q${QPS}.bin
